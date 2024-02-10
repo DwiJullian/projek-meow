175d7650b0947b01b0b2940d8545599113f4194c
JulianDapa <155231004+DwiJullian@users.noreply.github.com>
2024-02-10 11:35:40 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#103f94',
+        dark: '#0A101E',
+      },
+      fontFamily: {
+        Inter: ['Inter']
+      },
+    },
+  },
+  plugins: [],
+}
+
